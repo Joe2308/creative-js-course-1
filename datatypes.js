c34396792d4716a5b73b6e2cb620bad09dc955ff
Joe <joebreilly@yahoo.ie>
2022-03-29 22:45:07 +0000

    datatypes and operators

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+// Primitive data types 
+
+// Numbers 
+const day = 5;
+// Check the type of data we are using:
+console.log(typeof day);
+
+// Strings
+const name = "Joe";
+console.log(typeof name);
+
+// Booleans
+const isLogged = true;
+console.log(typeof isLogged);
+
+// Undefined
+let time;
+console.log(time);
+
+// Null
+let minute = null;
+console.log(null);
+
+// Symbol
+const sign = Symbol();
+console.log(sign);
---
+++ operators.js
@@ -0,0 +1,33 @@
+// Javascript operators
+
+// Basic operators
+
+let inbox = 10;
+// Add
+console.log(inbox + 10); 
+// Subtract
+console.log(inbox - 10);
+// Multiply
+console.log(inbox * 10);
+// Divide
+console.log(inbox / 10);
+// Modulos (remainder)
+console.log(inbox % 10);
+// Exponentiation (to the power of the 2nd number)
+console.log(inbox ** 10);
+
+// Increment by 1
+inbox++;
+console.log(inbox);
+
+// Decrement by 1
+inbox--;
+console.log(inbox);
+
+// Increment by a specific amount
+inbox += 20;
+console.log(inbox);
+
+// Decrement by a specific amount
+inbox -= 10;
+console.log(inbox);
