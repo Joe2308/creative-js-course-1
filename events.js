0bf5698d750b7910c751fe4236bcb4b3124e7ec7
Joe <joebreilly@yahoo.ie>
2022-04-19 22:01:58 +0000

    learning about events part 1

---
+++ events.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('#submit');
+const todoList = document.querySelector('#todo-list');
+const items = todoList.children;
+
+// Add event listener to element and run an anonymous function
+button.addEventListener('click', function () {
+    const newItem = document.createElement('li');
+    newItem.classList.add('item');
+    newItem.innerText = 'Item 3';
+    todoList.appendChild(newItem);
+})
+
+// We can also created an external function and reference it in the eventlistner
+
+// button.addEventListener('click', addItem);
+
+// function addItem() {
+//     console.log('newer item added');
+// }
