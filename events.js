2cc2bcdd0a2f84fd9b8874a405389ab1829a7467
Joe <joebreilly@yahoo.ie>
2022-05-08 09:24:34 +0000

    Learning about event bubbling and input values

--- events.js
+++ events.js
@@ -2,17 +2,28 @@ const button = document.querySelector('#submit');
 const todoList = document.querySelector('#todo-list');
 const todoNum = document.querySelector('.todo-nr');
 const mainTitle = document.querySelector('.main-title');
+const nameInput = document.querySelector(".name-input");
 const items = todoList.children;
 
 // Add event listener to element and run an anonymous function
-button.addEventListener('click', function () {
+button.addEventListener('click', function (e) {
+    // Prevent forms normal behaviour of refreshing on submit
+    e.preventDefault();
+    // Create element
     const newItem = document.createElement('li');
+    // Adding class
     newItem.classList.add('item');
-    newItem.innerText = `Item ${items.length +1} `;
+    // Adding the text
+    newItem.innerText = nameInput.value;
+    // Append the new element to ul
     todoList.appendChild(newItem);
     todoNum.innerText = items.length;
+    // Clear text from input field after it is submitted
+    nameInput.value = "";
     // Create the element and attach the listener
     newItem.addEventListener('click', deleteItem);
+
+    console.log(nameInput);
 });
 
 for (item of items) {
@@ -20,8 +31,14 @@ for (item of items) {
 }
 
 function deleteItem(e) {
-    console.log('delete', e.target);
+    // Prevent event bubbling
+    e.stopPropagation();
+    e.target.remove();
 }
+
+todoList.addEventListener('click', function () {
+    todoList.classList.toggle('fade');
+});
 // todoNr.innerText = items.length;
 // console.log(todoNr);
 // We can also created an external function and reference it in the eventlistner
