c3934d92ea7c82388707e5e96297f5f69f2ae6d1
Joe <joebreilly@yahoo.ie>
2022-05-10 22:16:58 +0000

    Learn about local storage and JSON commands

--- local-storage.js
+++ local-storage.js
@@ -1,18 +1,34 @@
-// Local Storage Remains after closing the browser
-// Local storage takes a key value pair as strings only
+// // Local Storage Remains after closing the browser
+// // Local storage takes a key value pair as strings only
 
-localStorage.setItem('todo', 'feed the cat');
+// localStorage.setItem('todo', 'feed the cat');
 
-// Session Storage deletes after closing browser
+// // Session Storage deletes after closing browser
 
-sessionStorage.setItem('todoList', 'session feeding cat');
+// sessionStorage.setItem('todoList', 'session feeding cat');
 
-// You have to manually clear local storage:
+// // You have to manually clear local storage:
+
+// // localStorage.clear();
+
+// // sessionStorage.clear();
+
+// // Getting stuff back:
+// const user = localStorage.getItem('todo');
+// console.log(typeof user);
 
 // localStorage.clear();
-
 // sessionStorage.clear();
 
-// Getting stuff back:
-const user = localStorage.getItem('todo');
-console.log(typeof user);
+// Now lets try to store an array to local storage
+const todoList = ['feed the cat', 'wash', 'listen to some music'];
+
+// Use the JSON.stringify method
+localStorage.setItem('todos', JSON.stringify(todoList));
+
+// Use JSON.parse method to return array to its oriinal form
+const retrieved = JSON.parse(localStorage.getItem('todos'));
+
+console.log(retrieved);
+
+// The above methods can be used with objects aswell
